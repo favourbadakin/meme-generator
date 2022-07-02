b78640f75608d9da82cffcd7e2b997049403fea5
favourbadakin <favourbadakin@gmail.com>
2022-07-02 14:21:37 +0100

    random commit

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Nav(){
+    return(
+            <nav>
+                    <div className='nav-items'>
+                <img src = '../images/Trollface.svg' alt='trollface'/>
+                   <h1 >Meme Generator</h1>
+                   </div>
+            </nav>
+    );
+}
